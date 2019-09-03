eff4242a0fab44b122af400756ec06715fc58d16
420190038 <420190038@LRNWNWRK-011.learn.cpp.ac.nz>
2019-09-03 14:04:27 +1200

    Super Final Build

--- src/expressionvalidation/ExpressionValidation.java
+++ src/expressionvalidation/ExpressionValidation.java
@@ -21,33 +21,38 @@ public class ExpressionValidation {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter expression to validate.");
+        System.out.println("Enter expression to validate:");
         String expression = sc.next();
         
-        System.out.println("Enter characters to validate against.");
+        System.out.println("Enter both characters to validate against - eg: () or {}.");
         String charPair = sc.next();
         Stack stack = new Stack();
         int a = 0;
-        
-        for (int i=0; i < expression.length(); i++) {
-            if (expression.charAt(i) == charPair.charAt(0)) {
-                stack.push(expression.charAt(i));
-            } else if (expression.charAt(i) == charPair.charAt(1)) {
-                if (stack.empty()) {
-                    a++;
-                    break;
-                } else {
-                    stack.pop();   
+        if (charPair.length() == 2) {
+            for (int i=0; i < expression.length(); i++) {
+                if (expression.charAt(i) == charPair.charAt(0)) {
+                    stack.push(expression.charAt(i));
+                } else if (expression.charAt(i) == charPair.charAt(1)) {
+                    if (stack.empty()) {
+                        a++;
+                        break;
+                    } else {
+                        stack.pop();   
+                    }
                 }
             }
+            if (stack.empty() && a == 0) {
+                System.out.println("\nValidation complete. This expression is acceptable.");
+            } else if (a > 0){
+                System.out.println("\nThere were too many closing brackets. Expression failed.");
+            }else{
+                System.out.println("\nThere were unclosed brackets. Expression failed.");
+            }
+        } else {
+            System.out.println("\nPlease enter two brackets as shown. EG: (), {}, [].\n");
+            ExpressionValidation.main(args);
         }
-        if (stack.empty() && a == 0) {
-            System.out.println("Validation complete. This expression is acceptable.");
-        } else if (a > 0){
-            System.out.println("There were too many closing brackets. Expression failed.");
-        }else{
-            System.out.println("There were unclosed brackets. Expression failed.");
-        }
+        
     }  
 }
     
